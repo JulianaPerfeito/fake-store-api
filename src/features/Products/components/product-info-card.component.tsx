c54b99eac203bdeaff5eebed3c1945b828020b99
JulianaPerfeito <julianaperfeito.s@gmail.com>
2023-08-29 11:34:21 -0300

    feat: add product to Cart

--- src/features/Products/components/product-info-card.component.tsx
+++ src/features/Products/components/product-info-card.component.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import {
   InfoContainer,
   ProductCard,
@@ -8,8 +9,15 @@ import {
 } from "./product-info-card.style";
 import { Button } from "react-native-paper";
 import Product from "../../../models/product";
+import ICartContext, { CartContext } from "../../../contexts/CartContext";
 
 export const ProductInfoCard = ({ product }: { product: Product }) => {
+  const { cart, addItemToCart } = useContext(CartContext) as ICartContext;
+
+  const handleClick = (product: Product) => {
+    addItemToCart(product);
+  };
+
   return (
     <ProductCard>
       <ProductCardCover source={{ uri: product.image }} />
@@ -17,7 +25,7 @@ export const ProductInfoCard = ({ product }: { product: Product }) => {
         <ProductNameText>{product.title}</ProductNameText>
         <CategoryText>{product.category}</CategoryText>
         <PriceText>${product.price}</PriceText>
-        <Button mode="contained" onPress={() => console.log("Pressed")}>
+        <Button mode="contained" onPress={() => handleClick(product)}>
           COMPRAR
         </Button>
       </InfoContainer>
