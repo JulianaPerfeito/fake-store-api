c333080bd069b596c2448bdf272eb587fcac6345
JulianaPerfeito <julianaperfeito.s@gmail.com>
2023-08-29 11:34:53 -0300

    feat: add a Card model as interface

---
+++ src/models/cart.ts
@@ -0,0 +1,14 @@
+export default interface Cart {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+  amount: number;
+  total: number;
+}
