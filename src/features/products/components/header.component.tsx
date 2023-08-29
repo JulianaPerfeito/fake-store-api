1023b5459aa767e36b7c2ddc034186227ed1dd74
JulianaPerfeito <julianaperfeito.s@gmail.com>
2023-08-29 00:16:36 -0300

    feat: onPress shopping-cart icon opens CartScreen

--- src/features/products/components/header.component.tsx
+++ src/features/products/components/header.component.tsx
@@ -2,11 +2,18 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { HeaderContainer, Title } from "./header.style";
 
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+interface ScreenNavigationProp {
+  navigate: (screen: string) => void;
+}
 export const Header = () => {
+  const { navigate } = useNavigation<ScreenNavigationProp>();
+
   return (
     <HeaderContainer>
       <Title>FAKE NATTY STORE</Title>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigate("CartScreen")}>
         <Feather name="shopping-cart" size={30} color="#000" />
       </TouchableOpacity>
     </HeaderContainer>
