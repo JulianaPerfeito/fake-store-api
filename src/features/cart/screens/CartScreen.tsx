12219ef2397f6d5c9a7c49766f6b179d37c18916
JulianaPerfeito <julianaperfeito.s@gmail.com>
2023-08-29 11:26:00 -0300

    feat: creat CartScreen to dysplay products add to buy

--- src/features/cart/screens/CartScreen.tsx
+++ src/features/cart/screens/CartScreen.tsx
@@ -1,4 +1,20 @@
-import { View, Text } from "react-native";
+import { useContext } from "react";
+import { CartInfoCard } from "./components/cart-info-card.component";
+import ICartContext, { CartContext } from "../../../contexts/CartContext";
+import { FlatList, View, Text } from "react-native";
+import Cart from "../../../models/cart";
+
 export const CartScreen = () => {
-  return <View></View>;
+  const { cart, total } = useContext(CartContext) as ICartContext;
+
+  return (
+    <View>
+      <FlatList
+        data={cart}
+        renderItem={({ item }) => <CartInfoCard cart={item} />}
+        keyExtractor={(item: Cart) => item.id.toString()}
+        ListFooterComponent={() => <Text>TOTAL: R${total.toFixed(2)}</Text>}
+      />
+    </View>
+  );
 };
